98dca51c61c1ccd596069c4445f3dc8b24271684
Michael Augustine <piyush.michael93@gmail.com>
2019-07-17 22:01:04 +0530

    contacts added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
-import {View,Text,Image,Button} from 'react-native';
+import {ScrollView,View,Text,Image,Button,PermissionsAndroid} from 'react-native';
 import ImagePicker from 'react-native-image-picker';
+import Contacts from 'react-native-contacts';
 /*--------------------------------*/
 import {styles} from './src/styles';
 
@@ -8,20 +9,34 @@ class App extends Component {
 	state = {avatar: ''};
 	
 	addAvatar = () => {
-		ImagePicker.showImagePicker({},response => {
-			if(response.didCancel){console.warn('really?')}
-			else if(response.error){console.warn(error)}
+		ImagePicker.showImagePicker({
+			title: 'you need an avatar',
+			takePhotoButtonTitle: 'click an avatar :)',
+			chooseFromLibraryButtonTitle: 'pick an avatar :)'
+		},response => {
+			if(response.didCancel){console.log('really?')}
+			else if(response.error != ''){console.log(error)}
 			else{this.setState({avatar: response.uri})}
 		});
+	};
+	launchCamera = () => {
+		ImagePicker.launchCamera({},response => {});
+	};
+	launchGallery = () => {
+		ImagePicker.launchImageLibrary({},response => {});
 	};
 	
 	render(){
 		return (
-		<View style={styles.container}>
-			<Text>started from the bottom now we hya :)</Text>
-			<Image source={{uri:this.state.avatar}} style={styles.avatar} />
-			<Button title="add an image" onPress={() => this.addAvatar()} />
-		</View>);
+		<ScrollView>
+			<View style={styles.container}>
+				<Text>started from the bottom now we hya :)</Text>
+				<Image source={{uri:this.state.avatar}} style={styles.avatar} />
+				<Button title="add an image" onPress={() => this.addAvatar()} />
+				<Button title="camera" onPress={() => this.launchCamera()}/>
+				<Button title="gallery" onPress={() => this.launchGallery()} />
+			</View>
+		</ScrollView>);
 	}
 };
 
--- android/app/src/main/java/com/nativeresources/MainApplication.java
+++ android/app/src/main/java/com/nativeresources/MainApplication.java
@@ -11,6 +11,7 @@ import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 
 import java.util.List;
 
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+	  new ReactNativeContacts();
       return packages;
     }
 
